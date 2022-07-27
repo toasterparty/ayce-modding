844825383412089ca2a35b62bc09b2c575850b5b
toasterparty <toasterparty@derpymail.org>
2022-07-27 12:59:53 -0700

    Rename config file to something less obscure

--- AyceModding/AyceModding.cs
+++ AyceModding/AyceModding.cs
@@ -1,18 +1,21 @@
-﻿using BepInEx;
+﻿using System.IO;
+using BepInEx;
 using BepInEx.IL2CPP;
 using BepInEx.Configuration;
-
 namespace AyceModding
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     [BepInProcess("Overcooked All You Can Eat.exe")]
     public class AyceModding : BasePlugin
     {
+
         private ConfigEntry<bool> configExampleBool;
 
         public override void Load()
         {
-            configExampleBool = Config.Bind(
+            var configFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "AyceModding.cfg"), true);
+
+            configExampleBool = configFile.Bind(
                 "Example", // Config Category
                 "ExampleBool", // Config key name
                 false, // Default Config value
@@ -20,6 +23,7 @@ namespace AyceModding
             );
 
             Log.LogInfo($"Plugin '{PluginInfo.PLUGIN_GUID}' loaded.");
+            Log.LogInfo($"Example Config = {configExampleBool.Value}");
         }
     }
 }
