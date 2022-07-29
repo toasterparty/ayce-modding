965a00baf3b07b3fb7fceba0b62b4598e976413c
toasterparty <toasterparty@derpymail.org>
2022-07-28 20:31:36 -0700

    Separate patch into it's own standalone module

--- AyceModding/AyceModding.cs
+++ AyceModding/AyceModding.cs
@@ -3,8 +3,6 @@ using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Configuration;
 using BepInEx.IL2CPP;
-using UnhollowerBaseLib;
-using HarmonyLib;
 
 namespace AyceModding
 {
@@ -13,56 +11,20 @@ namespace AyceModding
     public class AyceModding : BasePlugin
     {
         internal static new ManualLogSource Log;
-        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
-        private static ConfigFile configFile;
-        private static ConfigEntry<bool> configUnlockAllChefs;
+        public static ConfigFile configFile;
 
         public override void Load()
         {
+            AyceModding.Log.LogInfo($"AyceModding is alive!");
+
             /* Setup Logging */
             AyceModding.Log = base.Log;
 
             /* Initialize Configuration */
             configFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "AyceModding.cfg"), true);
-            bindConfig();
 
-            /* Inject Mods */
-            if (configUnlockAllChefs.Value)
-            {
-                Harmony.CreateAndPatchAll(typeof(PatchUnlockAllChefs));
-            }
-
-            AyceModding.Log.LogInfo($"Loaded Successfully");
-        }
-
-        private void bindConfig()
-        {
-            configUnlockAllChefs = configFile.Bind(
-                "Cosmetic", // Config Category
-                "UnlockAllChefs", // Config key name
-                true, // Default Config value
-                "Set to true to show all Chefs on the Chef selection screen" // Friendly description
-            );
-        }
-
-        class PatchUnlockAllChefs
-        {
-            [HarmonyPatch(typeof(ChefSelectMenu), "SetUpGridItem")]
-            [HarmonyPrefix]
-            public static bool Prefix(ref Il2CppStructArray<bool> variantsUnlocked, ref int firstUnlockedVariant, ref bool unlockable)
-            {
-                /* Unlock all variants */
-                for (int i = 0; i < variantsUnlocked.Count; i++)
-                {
-                    variantsUnlocked[i] = true;
-                }
-
-                /* Unlock all chefs */
-                firstUnlockedVariant = 0;
-                unlockable = true;
-
-                return true; // Execute original function
-            }
+            /* Initialize Components */
+            PatchUnlockAllChefs.load();
         }
     }
 }
---
+++ AyceModding/UnlockAllChefs.cs
@@ -0,0 +1,47 @@
+using BepInEx.Configuration;
+using UnhollowerBaseLib;
+using HarmonyLib;
+
+namespace AyceModding
+{
+    public class PatchUnlockAllChefs
+    {
+        private static ConfigEntry<bool> configUnlockAllChefs;
+
+        public static void load()
+        {
+            /* Setup Configuration */
+            configUnlockAllChefs = AyceModding.configFile.Bind(
+                "Cosmetic", // Config Category
+                "UnlockAllChefs", // Config key name
+                true, // Default Config value
+                "Set to true to show all Chefs on the Chef selection screen" // Friendly description
+            );
+
+            /* Inject Mods */
+            if (configUnlockAllChefs.Value)
+            {
+                AyceModding.Log.LogInfo($"Force unlocked all chefs");
+                Harmony.CreateAndPatchAll(typeof(PatchUnlockAllChefs));
+            }
+        }
+
+        [HarmonyPatch(typeof(ChefSelectMenu), "SetUpGridItem")]
+        [HarmonyPrefix]
+        public static bool Prefix(ref Il2CppStructArray<bool> variantsUnlocked, ref int firstUnlockedVariant, ref bool unlockable)
+        {
+            /* Unlock all variants */
+            for (int i = 0; i < variantsUnlocked.Count; i++)
+            {
+                variantsUnlocked[i] = true;
+            }
+
+            /* Unlock all chefs */
+            firstUnlockedVariant = 0;
+            unlockable = true;
+
+            return true; // Execute original function
+        }
+
+    }
+}
